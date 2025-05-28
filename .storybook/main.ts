934f00af1be53157ac83a55d2ce2a61e2b225168
Laís Batista <lais.batista.pereira@gmail.com>
2025-05-28 19:50:10 -0300

    feat: update Storybook configuration and add preview file with UIProvider.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,20 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import { createRequire } from "node:module";
+import { dirname, join } from "node:path";
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const require = createRequire(import.meta.url);
 
 const config: StorybookConfig = {
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
-  stories: ['../packages/ui/src/**/*.stories.@(ts|tsx)'],
+  // The Storybook configuration file for a React project using Vite
+  framework: getAbsolutePath("@storybook/react-vite"),
+  stories: [
+    "../packages/ui/src/**/*.mdx",
+    "../packages/ui/src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [getAbsolutePath("@storybook/addon-onboarding"), getAbsolutePath("@storybook/addon-docs")],
 };
-
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
