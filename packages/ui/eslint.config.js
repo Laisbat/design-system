bfebd178e6231716af9c380fd2c00d40648490ae
Laís Batista <lais.batista.pereira@gmail.com>
2025-05-28 19:53:37 -0300

    feat: add ESLint configuration for TypeScript and React with Storybook support.

---
+++ packages/ui/eslint.config.js
@@ -0,0 +1,48 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import tseslint from 'typescript-eslint';
+
+const tsConfig = {
+  files: ['**/*.ts', '**/*.tsx','**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx'],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+      project: './tsconfig.json',
+      tsconfigRootDir: process.cwd(),
+    },
+    globals: {
+      describe: 'readonly',
+      it: 'readonly',
+      expect: 'readonly',
+      beforeEach: 'readonly',
+      afterEach: 'readonly',
+      vi: 'readonly',
+    },
+  },
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    react,
+    'react-hooks': reactHooks,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+  },
+};
+
+export default [js.configs.recommended, // Ignora arquivos de config e infra
+{
+  ignores: ['eslint.config.js', '**/*.config.{js,ts}', 'vite.config.ts', 'tailwind.config.js', 'postcss.config.js'],
+}, tsConfig, prettier, ...storybook.configs["flat/recommended"]];
